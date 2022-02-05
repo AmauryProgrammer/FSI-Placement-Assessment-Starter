5ccf609807f12934d99e58d11cc1147697a6a945
AmauryProgrammer <amauryacosta1@gmail.com>
2022-02-05 17:51:02 -0500

    corrected code

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,122 @@
 let yourName = "Amaury Acosta"
-
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
-let ginger = 0 // Gingerbread
-
-
-
 const credit = document.querySelector('#credit')
 credit.textContent = `Created by ${yourName}`
 
+//set default values
+
+let gb = 0 // Gingerbread
+let cc = 0 // Chocolate Chip
+let ss = 0 // Sugar Sprinkle
+let total = 0
+
+//set variables for quantity
+
+var totalQuantity = document.getElementById('qty-total');
+var gbQuantity = document.getElementById('qty-gb');
+var ccQuantity = document.getElementById('qty-cc');
+var ssQuantity = document.getElementById('qty-sugar');
+
+
+//set event listener for Gingerbread cookie + button
+
+document.getElementById("add-gb").addEventListener("click", function() {
+    
+    {
+        gb++
+    
+   
+    gbQuantity.textContent = gb;
+    
+    total = total + 1;
+    }
+    totalQuantity.textContent = total;
+    
+    console.log("Gingerbread + was clicked!")
+})
+
+// set even listener for Gingerbread cookie - button
+
+document.getElementById("minus-gb").addEventListener("click", function() {
+    if(gb > 0)
+        {
+            gb--
+        
+       
+        gbQuantity.textContent = gb;
+    
+        total = total - 1;
+        }
+        totalQuantity.textContent = total;
+                
+        console.log("Gingerbread - was clicked!")
+    })
+
+//set event listener for Chocolate chip cookie + button
+
+document.getElementById("add-cc").addEventListener("click", function() {
+    
+    {
+        cc++
+    
+    
+    ccQuantity.textContent = cc;
+    
+    total = total + 1;
+    }
+    totalQuantity.textContent = total;
+    
+    console.log("Chocolate Chip + was clicked!")
+})
+
+//set event listener for Chocolate chip cookie - button
+
+document.getElementById("minus-cc").addEventListener("click", function() 
+{
+    if(cc > 0)
+    {
+        cc--
+
+    
+        
+    ccQuantity.textContent = cc;
+    total = total - 1;
+    }
+    totalQuantity.textContent = total;
+    
+    console.log("Chocolate Chip - was clicked!")
+})
+
+//set event listener for Sugar Sprinkle cookies + button
+
+document.getElementById("add-sugar").addEventListener("click", function() {
+    
+    {
+        ss++
+    
+    
+    ssQuantity.textContent = ss;
+    totalQuantity = document.getElementById("qty-total");
+    total = total + 1;
+    }
+    totalQuantity.textContent = total;
+    
+    console.log("Sugar Sprinkle + was clicked!")
+})
+
+//set event listener for Sugar Sprinkle cookies - button
+
+document.getElementById("minus-sugar").addEventListener("click", function() {
+    if ( ss > 0)
+    {
+        ss--
+       
+    
+    ssQuantity.textContent = ss;
+    
+    total = total - 1;
+    }
+    totalQuantity.textContent = total;
+    
+    console.log("Sugar Sprinkle - was clicked!")
+})
 
